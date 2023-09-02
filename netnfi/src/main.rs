bad0c8cd130be69b7b34cbf284357945e4688bd8
Alexander <Alexharttree@gmail.com>
2023-09-02 15:08:27 -0400

    Better args usage

--- netnfi/src/main.rs
+++ netnfi/src/main.rs
@@ -1,16 +1,29 @@
 extern crate libc;
+extern crate clap;
 
 use std::net::{IpAddr, Ipv4Addr};
 use std::ptr;
 use libc::{ifaddrs, sockaddr, sockaddr_in, AF_INET, getifaddrs, freeifaddrs};
+use clap::{App, Arg};
+
+fn is_interface_up(ifa_flags: libc::c_uint) -> bool {
+    // Convert libc::IFF_UP to u32 and then perform the bitwise AND operation
+    (ifa_flags & libc::IFF_UP as libc::c_uint) != 0
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let matches = App::new("netnfi")
+        .version("1.0")
+        .author("Your Name")
+        .about("Display network interface information")
+        .arg(Arg::with_name("mode")
+            .help("Display mode: brief or all")
+            .required(true)
+            .possible_values(&["brief", "all"])
+            .index(1))
+        .get_matches();
 
-    if args.len() != 2 || args[1] != "show" {
-        eprintln!("Usage: netnfi show");
-        std::process::exit(1);
-    }
+    let mode = matches.value_of("mode").unwrap();
 
     let mut ifa_list: *mut ifaddrs = ptr::null_mut();
     
@@ -22,12 +35,30 @@ fn main() {
     let mut ifa = ifa_list;
     while !ifa.is_null() {
         let ifa_name = unsafe { (*ifa).ifa_name };
-        
+        let ifa_flags = unsafe { (*ifa).ifa_flags };
+
         let family = unsafe { (*ifa).ifa_addr.as_ref().unwrap().sa_family as i32 };
         if family == AF_INET {
             let sockaddr_in = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in }) };
             let ip = IpAddr::V4(Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.s_addr)));
-            println!("Interface: {} - IP Address: {:?}", unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, ip);
+            let is_up = is_interface_up(ifa_flags);
+
+            match mode {
+                "brief" => {
+                    println!("Interface: {} - IP Address: {:?}", 
+                        unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                        ip
+                    );
+                }
+                "all" => {
+                    println!("Interface: {} - IP Address: {:?} - Status: {}", 
+                        unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                        ip, 
+                        if is_up { "Up" } else { "Down" }
+                    );
+                }
+                _ => unreachable!(), // This should never happen due to clap's argument validation
+            }
         }
 
         ifa = unsafe { (*ifa).ifa_next };
