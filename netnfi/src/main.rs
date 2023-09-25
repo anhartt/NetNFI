6b2904b08e9ea193d281bc3188cca0816972bef4
Alexander <Alexharttree@gmail.com>
2023-09-25 14:27:53 -0400

    Macro noun reworks

--- netnfi/src/main.rs
+++ netnfi/src/main.rs
@@ -30,85 +30,124 @@ fn main() {
         .version("1.0")
         .author("Alex Harttree")
         .about("Display network interface information")
-        .arg(Arg::with_name("mode")
-            .help("Display mode: brief, all, count, or ipv6")
-            .required(true)
-            .possible_values(&["brief", "all", "count", "ipv6"])
-            .index(1))
+        .subcommand(
+            App::new("show")
+                .about("Show network interface information")
+                .subcommand(App::new("brief").about("Show brief interface information"))
+                .subcommand(App::new("all").about("Show all interface information"))
+                .subcommand(App::new("ipv6").about("Show IPv6 interface information"))
+        )
+        .subcommand(
+            App::new("count")
+                .about("Count active network interfaces")
+                .subcommand(App::new("all").about("Count all active interfaces"))
+                // Add more count subcommands here for future expansion
+        )
         .get_matches();
 
-    let mode = matches.value_of("mode").unwrap();
+    let show_matches = matches.subcommand_matches("show");
+    let count_matches = matches.subcommand_matches("count");
 
-    let mut ifa_list: *mut ifaddrs = ptr::null_mut();
+    match show_matches {
+        Some(subcommand_matches) => {
+            if let Some(mode) = subcommand_matches.subcommand_name() {
+                let mut ifa_list: *mut ifaddrs = ptr::null_mut();
 
-    if unsafe { getifaddrs(&mut ifa_list) } == -1 {
-        panic!("Failed to get network interfaces");
-    }
-
-    let mut ifa = ifa_list;
-    while !ifa.is_null() {
-        let ifa_name = unsafe { (*ifa).ifa_name };
-        let ifa_flags = unsafe { (*ifa).ifa_flags };
-
-        let family = unsafe { (*ifa).ifa_addr.as_ref().unwrap().sa_family as i32 };
-        let is_up = is_interface_up(ifa_flags);
-
-        match mode {
-            "brief" => {
-                if family == AF_INET {
-                    let sockaddr_in = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in }) };
-                    let ip = IpAddr::V4(Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.s_addr)));
-                    println!("Interface: {} - IP Address: {:?}", 
-                             unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
-                             ip
-                    );
-                } else if family == AF_INET6 {
-                    let sockaddr_in6 = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in6 }) };
-                    let ip = IpAddr::V6(Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
-                    println!("Interface: {} - IPv6 Address: {:?}", 
-                             unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
-                             ip
-                    );
+                if unsafe { getifaddrs(&mut ifa_list) } == -1 {
+                    panic!("Failed to get network interfaces");
                 }
-            }
-            "all" => {
-                if family == AF_INET {
-                    let sockaddr_in = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in }) };
-                    let ip = IpAddr::V4(Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.s_addr)));
-                    println!("Interface: {} - IP Address: {:?} - Status: {}", 
-                             unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
-                             ip, 
-                             if is_up { "Up" } else { "Down" }
-                    );
-                } else if family == AF_INET6 {
-                    let sockaddr_in6 = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in6 }) };
-                    let ip = IpAddr::V6(Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
-                    println!("Interface: {} - IPv6 Address: {:?} - Status: {}", 
-                             unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
-                             ip, 
-                             if is_up { "Up" } else { "Down" }
-                    );
+
+                let mut ifa = ifa_list;
+                while !ifa.is_null() {
+                    let ifa_name = unsafe { (*ifa).ifa_name };
+                    let ifa_flags = unsafe { (*ifa).ifa_flags };
+
+                    let family = unsafe { (*ifa).ifa_addr.as_ref().unwrap().sa_family as i32 };
+                    let is_up = is_interface_up(ifa_flags);
+
+                    match mode {
+                        "brief" => {
+                            if family == AF_INET {
+                                let sockaddr_in = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in }) };
+                                let ip = IpAddr::V4(Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.s_addr)));
+                                println!("Interface: {} - IP Address: {:?}", 
+                                         unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                                         ip
+                                );
+                            } else if family == AF_INET6 {
+                                let sockaddr_in6 = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in6 }) };
+                                let ip = IpAddr::V6(Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
+                                println!("Interface: {} - IPv6 Address: {:?}", 
+                                         unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                                         ip
+                                );
+                            }
+                        }
+                        "all" => {
+                            if family == AF_INET {
+                                let sockaddr_in = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in }) };
+                                let ip = IpAddr::V4(Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.s_addr)));
+                                println!("Interface: {} - IP Address: {:?} - Status: {}", 
+                                         unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                                         ip, 
+                                         if is_up { "Up" } else { "Down" }
+                                );
+                            } else if family == AF_INET6 {
+                                let sockaddr_in6 = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in6 }) };
+                                let ip = IpAddr::V6(Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
+                                println!("Interface: {} - IPv6 Address: {:?} - Status: {}", 
+                                         unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                                         ip, 
+                                         if is_up { "Up" } else { "Down" }
+                                );
+                            }
+                        }
+                        "ipv6" => {
+                            if family == AF_INET6 {
+                                let sockaddr_in6 = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in6 }) };
+                                let ip = IpAddr::V6(Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
+                                println!("Interface: {} - IPv6 Address: {:?}", 
+                                         unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
+                                         ip
+                                );
+                            }
+                        }
+                        _ => unreachable!(),
+                    }
+
+                    ifa = unsafe { (*ifa).ifa_next };
                 }
+
+                unsafe { freeifaddrs(ifa_list) };
+            } else {
+                println!("Incomplete command. Please specify 'brief', 'all', or 'ipv6'.");
             }
-            "count" => {
-                let active_count = count_active_interfaces(ifa_list);
-                println!("Active Interfaces: {}", active_count);
-            }
-            "ipv6" => {
-                if family == AF_INET6 {
-                    let sockaddr_in6 = unsafe { &*(unsafe { (*ifa).ifa_addr as *const sockaddr_in6 }) };
-                    let ip = IpAddr::V6(Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
-                    println!("Interface: {} - IPv6 Address: {:?}", 
-                             unsafe { std::ffi::CStr::from_ptr(ifa_name).to_str().unwrap() }, 
-                             ip
-                    );
-                }
-            }
-            _ => unreachable!(),
         }
-
-        ifa = unsafe { (*ifa).ifa_next };
+        None => {
+            println!("Incomplete command. Please specify 'show' and one of: 'brief', 'all', or 'ipv6'.");
+        }
     }
 
-    unsafe { freeifaddrs(ifa_list) };
+    match count_matches {
+        Some(subcommand_matches) => {
+            if let Some(count_mode) = subcommand_matches.subcommand_name() {
+                if count_mode == "all" {
+                    let mut ifa_list: *mut ifaddrs = ptr::null_mut();
+
+                    if unsafe { getifaddrs(&mut ifa_list) } == -1 {
+                        panic!("Failed to get network interfaces");
+                    }
+
+                    let active_count = count_active_interfaces(ifa_list);
+                    println!("Active Interfaces: {}", active_count);
+                    unsafe { freeifaddrs(ifa_list) };
+                } else {
+                    println!("Invalid count subcommand. Supported subcommands: 'all'.");
+                }
+            } else {
+                println!("Incomplete count command. Please specify a subcommand (e.g., 'all').");
+            }
+        }
+        None => {}
+    }
 }
